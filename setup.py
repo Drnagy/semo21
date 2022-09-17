6d75558e0b11019a4e40478674b27d24b53cf04e
SEMOELKBER21 <93216241+SEMOELKBER21@users.noreply.github.com>
2022-09-17 16:49:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ try:
   from config import *
   os.system('pm2 start bot.py --name {} --interpreter python3.8 --interpreter-args -u'.format(BOT_ID))
 except Exception as e:
-  API_ID = 793178
-  API_HASH = '9f4461079f30757ca0a4c23e14bd523f'
+  API_ID = 12410826
+  API_HASH = '563d5c8290807e93d9bd3976467ad251'
 
   out ="""
-API_ID = 793178
-API_HASH = '9f4461079f30757ca0a4c23e14bd523f'
+API_ID = 12410826
+API_HASH = '563d5c8290807e93d9bd3976467ad251'
 """
   def Bot(TOKEN,method,data):
     url = "https://api.telegram.org/bot{}/{}".format(TOKEN,method)
